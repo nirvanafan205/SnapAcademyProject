5c1ee1f813794505d3793f809bc49ca5d5435a12
captain.peanutbutter495 <47827017+nirvanafan205@users.noreply.github.com>
2023-04-20 22:22:23 -0700

    added audio mouseover/mouseoff

--- main.js
+++ main.js
@@ -8,12 +8,50 @@ var closeTime = new Date();
 closeTime.setHours(17, 30, 0); // 8:00 PM
 
 // Check if the current time is between the opening and closing times
-if (now.getTime() >= openTime.getTime() && now.getTime() <= closeTime.getTime()) 
-{
+if (now.getTime() >= openTime.getTime() && now.getTime() <= closeTime.getTime()) {
   document.getElementById("status").innerHTML = "Njoy Games & Comics is currently open!";
 }
 
- else
-{
+else {
   document.getElementById("status").innerHTML = "Njoy Games & Comics is currently closed.";
 }
+
+//starts audio when hovered on
+function playAudio() 
+{
+  var audio = document.getElementById("audio");
+  audio.play();
+}
+
+//ends audio when hovered on
+function stopAudio()
+ {
+  var audio = document.getElementById("audio");
+  audio.pause();
+  audio.currentTime = 0;
+}
+
+
+/*
+var audio = document.getElementById("audio");
+var startTime;
+
+function playAudio() {
+  if (audio.paused) {
+    audio.currentTime = startTime;
+    audio.play();
+  }
+  else {
+    startTime = audio.currentTime;
+    audio.pause();
+  }
+}
+
+function pauseAudio() {
+  if (!audio.paused) {
+    startTime = audio.currentTime;
+    audio.pause();
+  }
+}
+
+*/
